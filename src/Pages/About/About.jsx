1ca78197a6c93bf98703c46bcc2b1671b60992e7
Mohamed-Elsayed-1 <mohamedelsayed55086@gmail.com>
2025-03-11 21:10:30 +0200

    `Added CSS import and updated About page content`

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,3 +1,10 @@
+import './About.css'
 export const About = () => {
-  return <div>About</div>;
+  return <section className='about'>
+    <div className="container">
+      <h1>About This App</h1>
+      <p>This app is all about booking safari desert tour in Dubai</p>
+      <p><strong>Version: 1.0.0</strong></p>
+    </div>
+  </section>;
 };
